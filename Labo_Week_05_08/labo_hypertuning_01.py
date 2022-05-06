cfe0dcf736d8eb46e0c4320e6504cc4f7ec7b021
Axel De Roeck <axelderoeck22@gmail.com>
2022-05-06 15:58:38 +0200

    Labo hypertuning

--- Labo_Week_05_08/labo_hypertuning_01.py
+++ Labo_Week_05_08/labo_hypertuning_01.py
@@ -1,34 +1,25 @@
-# Imports
 from sklearn import datasets
 from sklearn.model_selection import GridSearchCV
 from pprint import pprint
 from sklearn.ensemble import RandomForestClassifier
 
-# Laad het iris dataset in van sklearn
 iris_data = datasets.load_iris()
-# Data en target uit de dataset halen als X & Y
 X = iris_data.data
 Y = iris_data.target
 
-# Dictionary aanmaken met alle mogelijke hyperparameters voor de gridsearch
 model_parameters = {
     'n_estimators': [50, 150, 250],
     'max_features': ['sqrt', 0.25, 0.5, 0.75, 1.0],
     'min_samples_split': [2, 4, 6]
 }
 
-# Random Forest Classifier model aanmaken
 rfmodel = RandomForestClassifier()
 
-# Grid meta-estimator aanmaken
 clf = GridSearchCV(rfmodel, model_parameters, cv=5)
 
-# Train de grid meta-estimator voor het beste model te vinden
 model = clf.fit(X, Y)
 
-# Print de beste (gevonden) hyperparameters
 pprint(model.best_estimator_.get_params())
 
-# Predict op het model met de beste (gevonden) hyperparameters
 predictions = model.predict(X)
-print(predictions)
+pprint(predictions)
